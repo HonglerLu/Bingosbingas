3becd9a692036ba137ec0eb7c71ab5a82b1e479e
UnloafedBread <hollowpointdeveloper@gmail.com>
2026-01-30 09:38:31 -0300

    basic rendering

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ class PushStrat extends BaseEditStrat{
         var Desc = formObject.Desc
         
         PushNewBlock(formObject)
+        RenderBlocks()
     }
 }
 class EditStrat extends BaseEditStrat{
@@ -37,12 +38,28 @@ class EditStrat extends BaseEditStrat{
     }
 }
 
+const GridColumns = 3
+const Grid = document.getElementById("Grid")
 const EditModes = { Push: new PushStrat(), Edit: new EditStrat() }
 let EditStrategy = EditModes.Push
 let DataBlocks = []
 
 function RenderBlocks() {
-    
+    let Container = null
+    let NewNodes = []
+    Grid.replaceChildren()
+    for (let i = 0; i < DataBlocks.length; i++){
+        if (i % GridColumns == 0){
+            Container = document.createElement("div")
+            Container.classList.add("row")
+            Grid.appendChild(Container)
+            NewNodes.push(Container)
+        }
+        var Icon = document.createElement("div")
+        Icon.classList.add("col")
+        Icon.innerText = "teste"
+        Container.appendChild(Icon)
+    }
 }
 
 function PushNewBlock(Data) {
@@ -53,3 +70,5 @@ function PushNewBlock(Data) {
 document.getElementById("Submit").addEventListener('click', function(){
     EditStrategy.PushData()
 })
+
+RenderBlocks()
